eb3ad5f011d0e8039e6a46ab4a0b5096ac9005e8
Alvaro Montalvan <alvaro.montalvan@epn.edu.ec>
2026-05-10 00:23:52 -0500

    Correccion asociar provedor a un producto

--- src/main/java/dao/InsumoDaoHibernate.java
+++ src/main/java/dao/InsumoDaoHibernate.java
@@ -1,11 +1,27 @@
 package dao;
 
+import config.HibernateUtil;
 import model.Insumo;
+import org.hibernate.Session;
+
+import java.util.List;
 
 public class InsumoDaoHibernate extends GenericHibernateDao<Insumo, Long> implements InsumoDao {
 
     public InsumoDaoHibernate() {
         super(Insumo.class);
+    }
+
+    @Override
+    public List<Insumo> findAll() {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery(
+                    "select i from Insumo i " +
+                    "left join fetch i.proveedor " +
+                    "order by i.id",
+                    Insumo.class
+            ).getResultList();
+        }
     }
 }
 
